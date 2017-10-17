4c98f70bc3c0a07417ecc6b7de6be7ea0eaf71c9
gal <g.alanis.lobato@gmail.com>
2017-10-17 16:46:51 +0200

    Added more evaluation metrics and simplified plotting

--- R/assess_link_predictors.R
+++ R/assess_link_predictors.R
@@ -1,5 +1,5 @@
 
-globalVariables(c("frac_rem", "method", "prec", "perf", "links_rem", 
+globalVariables(c("frac_rem", "method", "perf", "avg_perf", "links_rem", 
                   "links_recov"))
 
 #' Performance evaluation of link predictors
@@ -18,6 +18,8 @@ globalVariables(c("frac_rem", "method", "prec", "perf", "links_rem",
 #' from \code{g} at random.
 #' @param epochs integer; Number of times each fraction of links is randomly
 #' removed.
+#' @param metric character; The metric to evaluate the link predictor(s) 
+#' performance. Should be one of 'recall@k', 'aupr', 'auroc' or 'avg_prec'.
 #' @param preserve_conn logical; Should network connectivity be preserved after 
 #' edge pruning? This is important for some prediction techniques that can only 
 #' be applied to connected topologies. To preserve connectivity, the network's 
@@ -31,7 +33,8 @@ globalVariables(c("frac_rem", "method", "prec", "perf", "links_rem",
 #' \item{epoch}{Epoch of link removal.}
 #' \item{frac_rem}{Fraction of links pruned at the specified epoch.}
 #' \item{links_rem}{Number of links pruned at the specified epoch.}
-#' \item{links_recov}{Number of removed links recoverd by the link predictor.}
+#' \item{perf}{The performance of the link predictor, according to the selected
+#' evaluation metric.}
 #' 
 #' @author Gregorio Alanis-Lobato \email{galanisl@uni-mainz.de}
 #' 
@@ -49,8 +52,12 @@ globalVariables(c("frac_rem", "method", "prec", "perf", "links_rem",
 #' @importFrom purrr map2_dbl
 #'
 prune_recover <- function(g, ..., probes = seq(0.1, 0.5, 0.1), epochs = 10,
-                          preserve_conn = FALSE, use_weights = FALSE){
+                          metric = "recall@k", preserve_conn = FALSE, 
+                          use_weights = FALSE){
   predictors <- list(...)
+  if(!(metric %in% c("recall@k", "aupr", "auroc", "avg_prec"))){
+    stop("The selected metric is not valid!")
+  }
   if(("lp_mce" %in% predictors || "lp_leig" %in% predictors || 
      "lp_isomap" %in% predictors) && !preserve_conn){
     stop(paste0("One or more of your link predictors requires a connected ",
@@ -75,12 +82,12 @@ prune_recover <- function(g, ..., probes = seq(0.1, 0.5, 0.1), epochs = 10,
                 epoch = rep(1:epochs, each = length(predictors)*length(probes)), 
                 frac_rem = rep(rep(probes, each = length(predictors)), epochs), 
                 links_rem = round(frac_rem * length(removable_links)), 
-                links_recov = numeric(col_len))
+                perf = numeric(col_len))
   
-  res$links_recov <- map2_dbl(as.list(res$method), as.list(res$links_rem),
-                               prune_predict_assess, g = g, 
-                               removable_links = removable_links)
-  
+  res$perf <- map2_dbl(as.list(res$method), as.list(res$links_rem),
+                       prune_predict_assess, 
+                       g = g, removable_links = removable_links, 
+                       metric = metric)
   return(res)
 }
 
@@ -89,22 +96,25 @@ prune_recover <- function(g, ..., probes = seq(0.1, 0.5, 0.1), epochs = 10,
 #' Given a network of interest, the name of a link predictor, a number of links 
 #' to prune from the network and a set of removable links, samples edges at 
 #' random from the removable set and prunes the network. Then applies the link 
-#' prediction method to the pruned topology and checks if the 
-#' top-\code{links_rem} ranked links are in the set of removed edges.
+#' prediction method to the pruned topology and evaluates the prediction by the
+#' given metric
 #' 
 #' @param g igraph; A network of interest.
 #' @param method character; The name of a link prediction function.
 #' @param links_rem integer; Number of links to prune from the network.
 #' @param removable_links igraph edge sequence; Set of edges that can be pruned.
+#' @param metric character; The metric to evaluate the link predictor(s) 
+#' performance. Should be one of 'recall@k', 'aupr', 'auroc' or 'avg_prec'.
 #' 
-#' @return The number of top-\code{links_rem} ranked links that are in the set
-#' of removed edges.
+#' @return The performance of the link predictor, according to the selected
+#' evaluation metric.
 #' 
 #' @author Gregorio Alanis-Lobato \email{galanisl@uni-mainz.de}
 #' 
 #' @importFrom igraph delete_edges
+#' @importFrom PRROC pr.curve roc.curve
 #' 
-prune_predict_assess <- function(g, method, links_rem, removable_links){
+prune_predict_assess <- function(g, method, links_rem, removable_links, metric){
   # Sample edges at random from the removable set
   edg <- sample(removable_links, links_rem)
   
@@ -113,27 +123,50 @@ prune_predict_assess <- function(g, method, links_rem, removable_links){
   
   # Predict links
   pred <- do.call(method, list(g = g_perturbed)) 
-  pred <- pred[1:links_rem, ]
   
-  # Determine the number of top predictions that are in the set of removed edges
-  recovered <- sum(g[from = pred$nodeA, to = pred$nodeB])
+  if(metric == "recall@k"){
+    pred <- pred[1:links_rem, ]
+    
+    # Determine the number of top predictions that are in the set of 
+    # removed edges
+    perf <- sum(g[from = pred$nodeA, to = pred$nodeB])/links_rem  
+  }else if(metric == "aupr"){
+    scores <- nrow(pred):1
+    classes <- g[from = pred$nodeA, to = pred$nodeB]
+    perf <- pr.curve(scores.class0 = scores, weights.class0 = classes, 
+                     sorted = T)$auc.integral
+  }else if(metric == "auroc"){
+    scores <- nrow(pred):1
+    classes <- g[from = pred$nodeA, to = pred$nodeB]
+    perf <- roc.curve(scores.class0 = scores, weights.class0 = classes, 
+                     sorted = T)$auc
+  }else if(metric == "avg_prec"){
+    scores <- nrow(pred):1
+    classes <- g[from = pred$nodeA, to = pred$nodeB]
+    pr <- pr.curve(scores.class0 = scores, weights.class0 = classes, 
+                     sorted = T, curve = T, minStepSize = 1)
+    rec_reaches_1 <- max(which(pr$curve[, 1] == 1))
+    perf <- mean(pr$curve[rec_reaches_1:(nrow(pr$curve) - 1), 2])
+  }
   
-  return(recovered)
+  return(perf)
 }
 
 #' Plot the performance evaluation of link predictors
 #' 
 #' Given the assessment of one or more link predictors with function 
-#' \code{prune_recover}, plots the precision curves of the analysed methods.
-#' Each point corresponds to the average precision for a specific fraction of
+#' \code{prune_recover}, plots the performance curves of the analysed methods.
+#' Each point corresponds to the average performance for a specific fraction of
 #' removed links across all considered epochs.
 #' 
 #' @param res tibble; The result of applying \code{prune_recover} to a network.
 #' @param colours character/numeric; A vector of colours to depict the 
 #' performance curve of each link prediction method. There should be as many
 #' colours as assessed link predictors.
+#' @param err character; Include or not error bars in the plot. It should be one
+#' of 'none', 'sd' or 'se'.
 #' 
-#' @return A ggplot with the precision curve(s) of the assesed prediction 
+#' @return A ggplot with the performance curve(s) of the assesed prediction 
 #' method(s).
 #' 
 #' @author Gregorio Alanis-Lobato \email{galanisl@uni-mainz.de}
@@ -146,144 +179,47 @@ prune_predict_assess <- function(g, method, links_rem, removable_links){
 #' # Define a set of colours to plot
 #' colours <- c("#8da0cb", "#fc8d62", "#66c2a5")
 #' 
-#' # Plot the precision curves of the considered link predictors
-#' perf <- plot_lp_precision(assessment, colours)
-#' 
-#' @export
-#' @importFrom dplyr %>% mutate group_by summarise
-#' @import ggplot2
-#'
-plot_lp_precision <- function(res, colours = NA){
-  if(any(is.na(colours))){
-    colours = seq_along(unique(res$method))
-  }else if(length(colours) != length(unique(res$method))){
-    stop("The number of link prediction methods and colours does not match!!!")
-  }
-  
-  p <- res %>% mutate(prec = links_recov/links_rem) %>% 
-    group_by(method, frac_rem) %>% 
-    summarise(perf = mean(prec)) %>% 
-    ggplot(aes_(~frac_rem, ~perf, colour = ~method)) + 
-    geom_line() + 
-    scale_colour_manual(values = colours) +
-    labs(x = "Fraction of removed links", 
-         y = "Precision", colour = "Predictor") + 
-    theme_bw() + theme(legend.background = element_blank(), 
-                       legend.position = "top")
-  
-  return(p)
-}
-
-#' Plot the performance evaluation of link predictors
-#' 
-#' Given the assessment of one or more link predictors with function 
-#' \code{prune_recover}, plots the precision curves of the analysed methods.
-#' Each point corresponds to the average precision for a specific fraction of
-#' removed links across all considered epochs. This function also plots error 
-#' bars corresponding to standard errors.
-#' 
-#' @param res tibble; The result of applying \code{prune_recover} to a network.
-#' @param colours character/numeric; A vector of colours to depict the 
-#' performance curve of each link prediction method. There should be as many
-#' colours as assessed link predictors.
-#' 
-#' @return A ggplot with the precision curve(s) of the assesed prediction 
-#' method(s).
-#' 
-#' @author Gregorio Alanis-Lobato \email{galanisl@uni-mainz.de}
-#' 
-#' @examples
-#' # Assess the performance of three link predictors applied to the Zachary 
-#' # Karate Club network
-#' assessment <- prune_recover(g = karate_club, "lp_cn", "lp_aa", "lp_pa")
-#' 
-#' # Define a set of colours to plot
-#' colours <- c("#8da0cb", "#fc8d62", "#66c2a5")
-#' 
-#' # Plot the precision curves of the considered link predictors
-#' perf <- plot_lp_precision_se(assessment, colours)
+#' # Plot the performance curves of the considered link predictors
+#' perf <- plot_lp_performance(assessment, colours, err = "sd")
 #' 
 #' @export
 #' @importFrom dplyr %>% mutate group_by summarise n
 #' @import ggplot2
 #' @importFrom stats sd
 #'
-plot_lp_precision_se <- function(res, colours = NA){
+plot_lp_performance <- function(res, colours = NA, err = "none"){
   if(any(is.na(colours))){
     colours = seq_along(unique(res$method))
   }else if(length(colours) != length(unique(res$method))){
     stop("The number of link prediction methods and colours does not match!!!")
   }
-  
-  p <- res %>% mutate(prec = links_recov/links_rem) %>% 
-    group_by(method, frac_rem) %>% 
-    summarise(perf = mean(prec), 
-              up_err = perf + sd(prec)/sqrt(n()), 
-              dw_err = perf - sd(prec)/sqrt(n())) %>% 
-    ggplot(aes_(~frac_rem, ~perf, colour = ~method)) + 
-    geom_line() + 
-    geom_errorbar(aes_(ymin = ~dw_err, ymax = ~up_err), width = 0.01) +
-    scale_colour_manual(values = colours) +
-    labs(x = "Fraction of removed links", 
-         y = "Precision", colour = "Predictor") + 
-    theme_bw() + theme(legend.background = element_blank(), 
-                       legend.position = "top")
-  
-  return(p)
-}
-
-#' Plot the performance evaluation of link predictors
-#' 
-#' Given the assessment of one or more link predictors with function 
-#' \code{prune_recover}, plots the precision curves of the analysed methods.
-#' Each point corresponds to the average precision for a specific fraction of
-#' removed links across all considered epochs. This function also plots error 
-#' bars corresponding to standard deviations.
-#' 
-#' @param res tibble; The result of applying \code{prune_recover} to a network.
-#' @param colours character/numeric; A vector of colours to depict the 
-#' performance curve of each link prediction method. There should be as many
-#' colours as assessed link predictors.
-#' 
-#' @return A ggplot with the precision curve(s) of the assesed prediction 
-#' method(s).
-#' 
-#' @author Gregorio Alanis-Lobato \email{galanisl@uni-mainz.de}
-#' 
-#' @examples
-#' # Assess the performance of three link predictors applied to the Zachary 
-#' # Karate Club network
-#' assessment <- prune_recover(g = karate_club, "lp_cn", "lp_aa", "lp_pa")
-#' 
-#' # Define a set of colours to plot
-#' colours <- c("#8da0cb", "#fc8d62", "#66c2a5")
-#' 
-#' # Plot the precision curves of the considered link predictors
-#' perf <- plot_lp_precision_sd(assessment, colours)
-#' 
-#' @export
-#' @importFrom dplyr %>% mutate group_by summarise
-#' @import ggplot2
-#' @importFrom stats sd
-#'
-plot_lp_precision_sd <- function(res, colours = NA){
-  if(any(is.na(colours))){
-    colours = seq_along(unique(res$method))
-  }else if(length(colours) != length(unique(res$method))){
-    stop("The number of link prediction methods and colours does not match!!!")
+  if(!(err %in% c("none", "sd", "se"))){
+    stop("The indicated error type is not valid!")
+  }
+  if(err == "none"){
+    res <- res %>% group_by(method, frac_rem) %>% 
+      summarise(avg_perf = mean(perf))
+    err_bar_width <- 0
+  }else if(err == "sd"){
+    res <- res %>% group_by(method, frac_rem) %>% 
+      summarise(avg_perf = mean(perf), 
+                up_err = avg_perf + sd(perf), 
+                dw_err = avg_perf - sd(perf))
+    err_bar_width <- 0.01
+  }else if(err == "se"){
+    res <- res %>% group_by(method, frac_rem) %>% 
+      summarise(avg_perf = mean(perf), 
+                up_err = avg_perf + sd(perf)/sqrt(n()), 
+                dw_err = avg_perf - sd(perf)/sqrt(n()))
+    err_bar_width <- 0.01
   }
   
-  p <- res %>% mutate(prec = links_recov/links_rem) %>% 
-    group_by(method, frac_rem) %>% 
-    summarise(perf = mean(prec), 
-              up_err = perf + sd(prec), 
-              dw_err = perf - sd(prec)) %>% 
-    ggplot(aes_(~frac_rem, ~perf, colour = ~method)) + 
+  p <- res %>% ggplot(aes_(~frac_rem, ~avg_perf, colour = ~method)) + 
     geom_line() + 
-    geom_errorbar(aes_(ymin = ~dw_err, ymax = ~up_err), width = 0.01) +
+    geom_errorbar(aes_(ymin = ~dw_err, ymax = ~up_err), width = err_bar_width) +
     scale_colour_manual(values = colours) +
     labs(x = "Fraction of removed links", 
-         y = "Precision", colour = "Predictor") + 
+         y = "Performance", colour = "Predictor") + 
     theme_bw() + theme(legend.background = element_blank(), 
                        legend.position = "top")
   
